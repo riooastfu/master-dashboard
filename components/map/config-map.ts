3b275e53d8d5b9cc85b92708ac50dc5d40c4d808
riooastfu <rio.alaska123@gmail.com>
2025-02-06 14:02:29 +0700

    add: add company coordinates into config-map

--- components/map/config-map.ts
+++ components/map/config-map.ts
@@ -2,5 +2,9 @@ export const coordinateMap: Record<string, [number, number]> = {
     "checkbox-CUS": [-1.0136004945888857, 110.23684340700402],
     "checkbox-JV": [-0.9890164744428489, 110.09631107415572],
     "checkbox-MAR KR": [-0.3569689673205261, 109.34309260946083],
-    "checkbox-MAR BA": [-2.7069012230956844, 104.46717743309858]
+    "checkbox-MAR BA": [-2.7069012230956844, 104.46717743309858],
+    "checkbox-BAS": [1.6136, 100.1091],
+    "checkbox-RMM": [0.7074, 99.1164],
+    "checkbox-DIS": [0.8077, 99.0902],
+
 };
